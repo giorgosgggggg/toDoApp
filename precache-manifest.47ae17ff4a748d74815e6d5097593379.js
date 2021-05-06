a9574bd5d45f7ca1bf685aff3d6480e0f0d11740
giorgosgggggg <gakidisgiorgos@windowslive.com>
2021-05-06 14:33:24 +0200

    Updates

---
+++ precache-manifest.47ae17ff4a748d74815e6d5097593379.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b487deadd60a3f3d5b3d2423204f088",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e90ab9799996443046a",
+    "url": "./static/css/main.f12cc627.chunk.css"
+  },
+  {
+    "revision": "520c613c45b83142c435",
+    "url": "./static/js/2.56096e32.chunk.js"
+  },
+  {
+    "revision": "7e90ab9799996443046a",
+    "url": "./static/js/main.f7256ede.chunk.js"
+  },
+  {
+    "revision": "baab4f6995ba25947e6f",
+    "url": "./static/js/runtime-main.f5f70450.js"
+  }
+]);
